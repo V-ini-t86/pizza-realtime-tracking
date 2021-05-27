5c11159c80661c464a43d74cabbaf7aecc04fc6b
Vinit Joshi <vinitjoshi.2345@gmail.com>
2021-05-27 17:52:53 +0530

    we add larave-mix set up the tailwind css

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const expressLayout = require("express-ejs-layouts");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 // app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
